11679dc0f8ca44312d28c5141ed418d52c311268
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-02-24 19:22:14 +0330

    feat:dinamic search/finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
 import UserCard from "./components/UserCard";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const inputRef = useRef(null);
+  const [searchResult, setSearchResult] = useState([]);
+
   useEffect(() => {
     if (!localStorage.getItem("users")) {
       fetch("https://randomuser.me/api/?results=50")
@@ -19,17 +20,52 @@ function App() {
     }
   }, []);
 
+  function searchChangeHandler() {
+    setSearchResult(
+      users.filter(
+        (item) =>
+          item.name.title
+            .toLowerCase()
+            .includes(inputRef.current.value.toLowerCase()) ||
+          item.name.first
+            .toLowerCase()
+            .includes(inputRef.current.value.toLowerCase()) ||
+          item.name.last
+            .toLowerCase()
+            .includes(inputRef.current.value.toLowerCase()) ||
+          item.cell.includes(inputRef.current.value)
+      )
+    );
+  }
+
   return (
     <>
+      <div className="inputContainer">
+        <input
+          ref={inputRef}
+          onChange={searchChangeHandler}
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
       <div className="userCardContainer">
-        {users.map((user) => (
-          <UserCard
-            key={`${user.id.value ?? user.name.first}-${user.cell} `}
-            imgObj={user.picture}
-            name={user.name}
-            cell={user.cell}
-          />
-        ))}
+        {searchResult.length === 0
+          ? users.map((user) => (
+              <UserCard
+                key={`${user.id.value ?? user.name.first}-${user.cell} `}
+                imgObj={user.picture}
+                name={user.name}
+                cell={user.cell}
+              />
+            ))
+          : searchResult.map((user) => (
+              <UserCard
+                key={`${user.id.value ?? user.name.first}-${user.cell} `}
+                imgObj={user.picture}
+                name={user.name}
+                cell={user.cell}
+              />
+            ))}
       </div>
     </>
   );
