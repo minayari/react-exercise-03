e4ea82f96f15f49a5dbc655759a3b8399f9f05d8
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-02-25 18:50:47 +0330

    feat:single contact page

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import Contact from "../../pages/Contact";
+
 export default function UserCard({ imgObj, name, cell }) {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/contact", { state: { imgObj, name, cell } });
+  }
+
   return (
     <>
-      <div className="userCardContainer__item">
+      <div onClick={navigateHandler} className="userCardContainer__item">
         <img src={imgObj.large} alt="user-image" />
         <h3>
           {name.title}.{name.first} {name.last}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export default function Contact() {
-  return <h1>contact</h1>;
+  const location = useLocation();
+  const { imgObj, name, cell } = location.state || {};
+
+  return (
+    <div className="singelContact">
+      <img src={imgObj.large} />
+      <h2>{name.title}.{name.first} {name.last}</h2>
+      <h4>{cell}</h4>
+    </div>
+  );
 }
